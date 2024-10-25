013e0b473c0803b12b890c2de7b04443fdfc85b5
Smukherjee635 <sohammukherjee635@gmail.com>
2024-10-24 19:20:08 -0700

    Add files via upload
    Continued work on Implementation 1. Haven't finished yet

--- MazeRunner.m
+++ MazeRunner.m
@@ -1,29 +1,52 @@
 % brick = ConnectBrick('Senchariot');
 global key;
-
+global brick;
 InitKeyboard();
+redCount = 3;
+
+%brick.SetColorMode(1, 2)
+color = brick.ColorCode(1);
+disp(color);
+
 while 1
-    pause(0.1);
+    pause(0.05);
+    color = brick.UltrasonicDist(2);
+    display(distance);
+
+    distance = brick.ColorCode(1);
+    %disp(color);
+
+    if color == 5
+        redCount = redCount+1;
+        %pause(0.2);
+    end
+
     switch key
         case 'q'
-            brick.MoveMotor('ABD', 0);
+            brick.MoveMotor('AB', 0);
+            break;
+        case 0
+            if distance < 20
+                if redCount == 3
+                    brick.MoveMotor('B', -18);
+                    brick.MoveMotor('A', 18);
+                    pause(0.5);
+                    brick.MoveMotor('AB', 0);
+                else
+                    brick.MoveMotor('B', 18);
+                    brick.MoveMotor('A', -18);
+                    pause(0.5);
+                    brick.MoveMotor('AB', 0);
+                end
+                disp("loop 1")
+            else
+                disp("moving forward")
+                brick.MoveMotor('AB', -500);
+                %pause(0.5);
+            end
+
     end
 
-    % Read distance from ultrasonic sensor
-    distance = brick.UltrasonicDist(1); % Assuming port 2 is where the ultrasonlic sensor is connected
-    display(distance); % Display distance
 
-    % Check if the car is too close to a wall (threshold set to 20 cm)
-    if distance < 20
-        disp('Wall detected! Turning right...');
-
-        brick.MoveMotor('AB', 0); % Stop the car
-        pause(0.5); % Pause briefly before turning
-
-        brick.MoveMotor('B', 300); % Turn right by moving only the right motor (adjust duration as needed)
-        pause(0.5); % Adjust time to control how much it turns
-    else
-        % Move forward if no wall is detected
-        brick.MoveMotor('AB', 300); % Move forward
-    end
 end
+brick.MoveMotor('AB',0);
