aab5f0a905744bf010e79afc2cfb3d17b391ba51
Smukherjee635 <sohammukherjee635@gmail.com>
2024-10-29 16:53:57 -0700

    Update MazeRunner.m - (10/29/2024)

--- MazeRunner.m
+++ MazeRunner.m
@@ -1,52 +1,40 @@
-% brick = ConnectBrick('Senchariot');
 global key;
-global brick;
 InitKeyboard();
-redCount = 3;
-
-%brick.SetColorMode(1, 2)
-color = brick.ColorCode(1);
-disp(color);
 
 while 1
-    pause(0.05);
-    color = brick.UltrasonicDist(2);
-    display(distance);
+    pause(0.5);
 
-    distance = brick.ColorCode(1);
-    %disp(color);
+    distance = brick.UltrasonicDist(2);
+    disp(distance);
 
-    if color == 5
-        redCount = redCount+1;
-        %pause(0.2);
-    end
+    color = brick.ColorCode(1);
+    disp(color);
 
     switch key
+        case 0
+            brick.MoveMotor('AB', 0);
+
+            
+
+
         case 'q'
             brick.MoveMotor('AB', 0);
             break;
-        case 0
-            if distance < 20
-                if redCount == 3
-                    brick.MoveMotor('B', -18);
-                    brick.MoveMotor('A', 18);
-                    pause(0.5);
-                    brick.MoveMotor('AB', 0);
-                else
-                    brick.MoveMotor('B', 18);
-                    brick.MoveMotor('A', -18);
-                    pause(0.5);
-                    brick.MoveMotor('AB', 0);
-                end
-                disp("loop 1")
-            else
-                disp("moving forward")
-                brick.MoveMotor('AB', -500);
-                %pause(0.5);
-            end
+        case 'leftarrow'
+            brick.MoveMotor('A', 100);
+            brick.MoveMotor('B', -100);
 
+        case 'rightarrow'
+            brick.MoveMotor('A', -100);
+            brick.MoveMotor('B', 100);
+            
+        case 'uparrow'
+            brick.MoveMotor('A', -100);
+            brick.MoveMotor('B', -100);
+                      
+        case 'downarrow'
+            brick.MoveMotor('A', 100);
+            brick.MoveMotor('B', 100);
+              
     end
-
-
 end
-brick.MoveMotor('AB',0);
