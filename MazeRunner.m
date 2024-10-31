fed240076cb18ea4de491276a9f2a96818fba667
Smukherjee635 <sohammukherjee635@gmail.com>
2024-10-31 16:17:30 -0700

    Update MazeRunner.m

--- MazeRunner.m
+++ MazeRunner.m
@@ -1,24 +1,45 @@
+
+
 global key;
 InitKeyboard();
+redLineCount = 0;
 
 while 1
-    pause(0.5);
-
+    pause(0.05);
+    
     distance = brick.UltrasonicDist(2);
-    disp(distance);
+    fprintf('Distance : %f\n', distance);
 
     color = brick.ColorCode(1);
-    disp(color);
+    fprintf('Color : %f\n', color);
 
     switch key
         case 0
-            brick.MoveMotor('AB', 0);
+            brick.MoveMotor('AB', -100);
 
-            
+            if color == 5
+                 redLineCount = redLineCount + 1;
+                 brick.playTone(100, 440, 700);
+            end
 
+
+            if distance < 15
+                if redLineCount > 3
+                    brick.MoveMotor('A', -50);
+                    brick.MoveMotor('B', 50);
+                    pause(0.5);
+
+                else
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', -50);
+                    pause(0.5);
+                end
+
+            end
 
         case 'q'
             brick.MoveMotor('AB', 0);
+            CloseKeyboard();
             break;
         case 'leftarrow'
             brick.MoveMotor('A', 100);
@@ -37,4 +58,6 @@ while 1
             brick.MoveMotor('B', 100);
               
     end
+
+    fprintf('Red color counter : %f\n', redLineCount);
 end
